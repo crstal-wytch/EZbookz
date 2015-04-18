2683ef78a5722841fd2a114c1f85fd39bee2bee6
crstal-wytch <elaine.jordan@hotmail.com>
2015-04-18 15:02:32 +0000

    update for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,9 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  
+  # send email
+  config.action_mailer.default_url_options = { :host => '<app name>.herokuapp.com'}
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.perform_deliveries = true
 end
