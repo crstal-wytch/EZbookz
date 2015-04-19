8506336cbb46a07f0709b4f12fae383af467d712
crstal-wytch <elaine.jordan@hotmail.com>
2015-04-19 13:49:57 +0000

    changes

--- config/routes.rb
+++ config/routes.rb
@@ -9,7 +9,7 @@ Rails.application.routes.draw do
     collection { post :import }
   end
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  devise_for :users, :controllers => { registrations: 'registrations' } 
   get 'welcome/index'
   get 'welcome/about'
   get 'welcome/contact'
