e65c8d4ddf2166c0df1e83e61ca81e56e885a215
crstal-wytch <elaine.jordan@hotmail.com>
2015-04-24 23:12:44 +0000

    fix heroku

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -9,19 +9,7 @@ class CartsController < ApplicationController
   end
 
   def show
-    begin
-      @cart = Cart.find(params[:id])
-    rescue ActiveRecord::RecordNotFound
-      logger.error "Attempt to access invalid cart #{params[:id]}"
-    redirect_to items_path, notice: 'Invalid cart'
-  else
-    respond_to do |format|
-      format.html #show.html.erb
-      format.json {render json: @cart}
-    end
-  end
-  
-    #respond_with(@carts)  
+    respond_with(@carts)  
   end
 
   def new
